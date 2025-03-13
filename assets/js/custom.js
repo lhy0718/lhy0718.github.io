0d23570d0d62ae341225750fb5b7bd986f8c1f1b
LEE HANYONG <glhy0718@gmail.com>
2025-03-13 10:58:04 +0900

    taxonomy 섹션 표시 로직 개선: 페이지 로드 시 선택된 섹션 자동 표시 및 해시 변경 시 동작 수정

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,36 +11,45 @@ document.addEventListener("DOMContentLoaded", function () {
     })
   })
 
-  $(".taxonomy__section").hide()
-  selected_id = window.location.href.split("#")[1]
-  document.getElementById(selected_id).scrollIntoView();
-  $(".taxonomy__section").each(function () {
-    if ($(this).attr("id") === selected_id) {
-      $(this).show()
-    }
-  })
+  url = window.location.href
+  if (url.includes("tags") || url.includes("categories") || url.includes("posts")) {
 
-  window.onhashchange = function () {
-    selected_id = window.location.href.split("#")[1]
-    document.getElementById(selected_id).scrollIntoView();
     $(".taxonomy__section").hide()
-    $(".taxonomy__section").each(function () {
-        if ($(this).attr("id") === selected_id) {
-          $(this).show()
-        }
-      }
-    )
-  }
 
-  for (i = 0; i < $(".taxonomy__index li").length; i++) {
-     $(".taxonomy__index li")[i].onclick = function () {
-      selected = $(this).find("a").attr("href").split("#")[1]
+    window.onload = function () {
+      selected_id = window.location.href.split("#")[1]
+      document.getElementById(selected_id).scrollIntoView();
       $(".taxonomy__section").hide()
       $(".taxonomy__section").each(function () {
-        if ($(this).attr("id") === selected) {
-          $(this).show()
+          if ($(this).attr("id") === selected_id) {
+            $(this).show()
+          }
         }
-      })
-     }
+      )
+    }
+
+    window.onhashchange = function () {
+      selected_id = window.location.href.split("#")[1]
+      document.getElementById(selected_id).scrollIntoView();
+      $(".taxonomy__section").hide()
+      $(".taxonomy__section").each(function () {
+          if ($(this).attr("id") === selected_id) {
+            $(this).show()
+          }
+        }
+      )
+    }
+
+    for (i = 0; i < $(".taxonomy__index li").length; i++) {
+      $(".taxonomy__index li")[i].onclick = function () {
+        selected = $(this).find("a").attr("href").split("#")[1]
+        $(".taxonomy__section").hide()
+        $(".taxonomy__section").each(function () {
+          if ($(this).attr("id") === selected) {
+            $(this).show()
+          }
+        })
+      }
+    }
   }
 })
